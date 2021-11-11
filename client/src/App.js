ea07447f99f3baf9e70f7d78e4c65d833aa2d602
NIDHIRAGHU <90337066+NIDHIRAGHU@users.noreply.github.com>
2021-11-11 13:03:21 -0500

    heropane layout (#2)
    * heropane layout
    
    * redirected page layout | Landing Footer and Hero pane
    
    * Services page | Layout
    
    * services folder in project | subheader fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,66 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Content from './components/layout/Content';
+import MenuBar from './components/layout/MenuBar';
+import Footer from './components/layout/Footer';
+import React, { useEffect } from 'react';
+import Layout from './components/layout/Layout';
+import { useHistory, useLocation } from 'react-router';
+import ServiceProvider from './components/layout/ServiceProvider';
+import Header from './components/layout/Header';
+import ErrorComponent from './components/utilities/ErrorComponent';
+
 
 function App() {
+
+  const routepaths = ["home", "services", "seek"];
+  const history = useHistory();
+
+  useEffect(() => {
+
+    if (routepaths.includes(window.location.href.split()[3])) {
+      history.push({
+        pathname: "/"
+      });
+    }
+
+  }, [window.location.href])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router>
+        <Switch>
+          <Route exact path="/home" render={() => (
+            <React.Fragment>
+              <MenuBar component={<Content />} />
+              <Footer />
+            </React.Fragment>
+          )}>
+          </Route>
+
+          <Route path="/services" render={() => (
+            <Layout component={<Content />} />
+          )}>
+          </Route>
+
+          <Route path="/seek" render={() => (
+            <Layout component={<ServiceProvider />} />
+          )}>
+          </Route>
+
+          <Route path="/" render={() => (
+            <>
+              <Header hideSide={true} />
+              <ErrorComponent />
+            </>
+          )}>
+
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
   );
 }
---
+++ client/src/components/layout/Layout.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import Content from './Content'
+import Footer from './Footer'
+import Header from './Header'
+import SubHeader from './SubHeader'
+import '../../styles/layout.scss';
+import { Breadcrumb } from 'react-bootstrap'
+
+function Layout(props) {
+
+
+    useEffect(() => {
+       
+    }, [])
+
+    return (
+        <div className="main-layout">
+
+            <Header />
+            <SubHeader />
+            {props.component}
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout
