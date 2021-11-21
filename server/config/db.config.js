1fa5d815b4aa2632ad811d41d8788af9029d9a20
Pranjal Gupta <gupta.pran@northeastern.edu>
2021-11-21 18:54:53 -0500

    Export config file from db.config.js

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
     URI: "mongodb+srv://admin:9iSItxjYX4E3ByNH@cluster0.6u3xf.mongodb.net/mytripdb?retryWrites=true&w=majority",
     PORT: 27017,
     DB: "mytripdb",
